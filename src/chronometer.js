711e155f8a37eaa6d305c65019a1ff037487adaf
asmaaelhadad <asmaaelhadad@gmail.com>
2023-01-17 17:10:44 +0100

    Done

--- src/chronometer.js
+++ src/chronometer.js
@@ -1,33 +1,53 @@
 class Chronometer {
   constructor() {
-    // ... your code goes here
+ this.currentTime = 0;
+ this.intervalId= null;
   }
 
   start(printTimeCallback) {
-    // ... your code goes here
-  }
+   this.intervalId=  setInterval ( ()=>  {
+        this.currentTime += 1 ;
+     if (   printTimeCallback){ printTimeCallback();}
 
-  getMinutes() {
-    // ... your code goes here
+     }, 1000) ;
+         }
+
+  getMinutes() { 
+    
+  
+    return Math.floor(this.currentTime /60)  ;      
   }
 
   getSeconds() {
-    // ... your code goes here
+
+         let x =  (this.currentTime % 60);
+    return x ;
+     
   }
 
   computeTwoDigitNumber(value) {
-    // ... your code goes here
-  }
+
+    if (value===0){return '00';}
+         else if (value <10) {
+             return `0${value}` ;}
+                  else { return value.toString() ;}
+
+         }
 
   stop() {
-    // ... your code goes here
+    
+    clearInterval(this.intervalId);
+
   }
 
   reset() {
-    // ... your code goes here
+    clearInterval(this.currentTime);
+ this.currentTime = 0 ;
   }
 
   split() {
-    // ... your code goes here
+   let minutes= this.computeTwoDigitNumber(this.getMinutes());
+   let seconds= this.computeTwoDigitNumber(this.getSeconds()) ;
+   return `${minutes}:${seconds}` ;
   }
 }
